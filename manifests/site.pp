d9b06869fc2100b153852ff8e32bbf0e6dfc3795
Sahitya Maruvada <sahitya.lalitha@gmail.com>
2018-11-18 16:11:10 -0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,7 @@
 node default {
   file { '/root/README':
-        ensure => file,
-        content => 'This is a README only Site',
-        }
+    ensure => file,
+    content => 'This is a readme',
+    owner   => 'root',
+  }
 }
